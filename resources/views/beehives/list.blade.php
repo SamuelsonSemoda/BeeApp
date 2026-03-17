eb163fcaae3cd6acc3ddb4c118c54cbe44c381b2
samuel.semoda <samuel.semoda@orbiszlin.cz>
2026-03-17 09:06:07 +0100

    Version 0.1.3

--- resources/views/beehives/list.blade.php
+++ resources/views/beehives/list.blade.php
@@ -1,5 +1,6 @@
 <main>
     <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/global.css') }}">
     <h1>Úly – {{ $slug }}</h1>
 
